3526e30de98e278c5aec1bca259544f94c918a50
Rúben Israel <87450219+Ruben-974@users.noreply.github.com>
2024-01-27 21:54:42 -0300

    att

---
+++ Testes/Teste.py
@@ -0,0 +1,18 @@
+x = 0
+
+def sum_numbers(x):
+
+    def sum(y):
+
+        nonlocal x
+
+        x += y
+
+        return x
+
+    return sum
+
+ 
+sum_in_nine = sum_numbers(9)
+print(sum_in_nine(4))
+print(sum_in_nine(7))
