720986a31aaaba6ab7820f113bdf6eadc47aeb0e
Rúben Israel <87450219+Ruben-974@users.noreply.github.com>
2025-04-09 20:14:01 -0300

    .

---
+++ 2022/secao_04/a174/arq01.py
@@ -0,0 +1,26 @@
+# Combinations, Permutations and Product - Itertools
+# Combination - Order not important
+# Permutation - order matters
+# Product - Order imports and repeats unique values
+
+from itertools import combinations, permutations, product
+
+
+def format(iterable):
+    print(*iterable, sep='\n')
+    print()
+
+
+
+person = ['Jack', 'Anne', 'Julie', 'Marks']
+shirt = [['white', 'black'],
+         ['P', 'M', 'G'],
+         ['Man', 'Woman', 'unisex']]
+
+combination_persons = combinations(person, 2)
+format(combination_persons)
+combination_persons = permutations(person, 2)
+format(combination_persons)
+combination_shirt = product(*shirt)
+format(combination_shirt)
+
